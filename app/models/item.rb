8fdc231efb2a4a9a3d39c4e30b9367c3e1b99cc9
marksmerritt <markmerritt804@gmail.com>
2018-02-06 19:47:22 -0500

    comments

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,7 @@ class Item < ApplicationRecord
 
   default_scope { order(created_at: :asc) }
 
+  # while this method will work, think about being more flexible (same as partial). Add hours/min/secs. think about how to do this. 
   def days_left
     7 - (DateTime.now.to_date - created_at.to_date).to_i
   end
